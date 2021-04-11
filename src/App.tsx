317faa8281da42227dd029cb3e4b519c537bca82
872078694 <34040458+872078694@users.noreply.github.com>
2021-04-11 00:41:06 -0500

    design chart 1/n

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { Helmet } from 'react-helmet';
 import { HelloWorld } from './components/HelloWorldExample';
+import { DemoChart } from './pages/BarragePage/DemoChart';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
        <Helmet titleTemplate="%s | CryptoCommunity" defaultTitle="CryptoCommunity" />
        <Switch>
            <Route exact path="/" component={HelloWorld} />
-           <Route path="/login" component={()=><div/>} />
+           <Route path="/test" component={DemoChart} />
            <Route path="/register" component={()=><div/>} />
         </Switch>
       </BrowserRouter>
--- src/pages/BarragePage/DemoChart.tsx
+++ src/pages/BarragePage/DemoChart.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from 'highcharts-react-official';
+import BitCoinData from './BitCoinData.json'
 
-function DemoChart () {
-    return <div/>
+export function DemoChart () {
+    const options = {
+        title: {
+          text: 'My chart'
+        },
+        series: [{
+          data: BitCoinData,
+          color: '#00FA9A',
+        }],
+        credits: {
+            enabled: false
+        },
+        yAxis: {
+            gridLineWidth: 0,
+            title: null,
+            "startOnTick": true
+        },
+        xAxis: {
+            lineWidth: 1,
+            tickWidth: 0,
+        }
+      }
+    return <HighchartsReact
+    highcharts={Highcharts}
+    options={options}
+  />
 }
